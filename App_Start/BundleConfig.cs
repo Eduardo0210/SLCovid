5138c942f41be0011ea3dc411d1b5823784f8cbb
Luis Eduardo Alcantara Ibarra <edu.iwnwa@gmail.com>
2022-02-26 22:24:31 -0600

    Add all changes to desktop view

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -19,9 +19,11 @@ namespace SLCovid
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(""));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/bundles/jquery/svg-master").Include(
